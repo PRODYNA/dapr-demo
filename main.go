a3be71dc79e0bdd6ae19783cb811c12b9f77d5df
Darko Krizic <darko@krizic.net>
2022-03-10 11:11:19 +0100

    First pipeline

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
+	dapr "github.com/dapr/go-sdk/client"
 	"github.com/gorilla/mux"
 	muxlogrus "github.com/pytimer/mux-logrus"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"time"
-	dapr "github.com/dapr/go-sdk/client"
 )
 
 const (
@@ -35,12 +35,14 @@ func main() {
 
 func ScheduleHandler(w http.ResponseWriter, r *http.Request) {
 	log.WithField("url", r.URL.Path ).Info("Schedule triggered")
-	err := sendMessage()
-	if err != nil {
-		log.WithError(err).Warn("Unable to send message")
-		w.WriteHeader( 500 )
-		w.Write( []byte("error"))
-		return
+	if r.Method == "POST" {
+		err := sendMessage()
+		if err != nil {
+			log.WithError(err).Warn("Unable to send message")
+			w.WriteHeader(500)
+			w.Write([]byte("error"))
+			return
+		}
 	}
 	w.WriteHeader( 200 )
 	w.Write( []byte( "ok" ) )
