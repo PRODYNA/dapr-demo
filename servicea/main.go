e72e37bed88108dead84d21c9c713b2b44deb177
Darko Krizic <darko@krizic.net>
2022-06-08 13:49:26 +0200

    Invoking number service

--- servicea/main.go
+++ servicea/main.go
@@ -8,6 +8,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -59,7 +60,17 @@ func HealthHandler(w http.ResponseWriter, r *http.Request) {
 func CheckoutHandler(w http.ResponseWriter, r *http.Request) {
 	log.WithField("url", r.URL.Path).Info("Schedule triggered")
 	if r.Method == "POST" {
-		err := sendMessage()
+
+		// get order number
+		number, err := getOrderNumber()
+		if err != nil {
+			log.WithError(err).Error("Unable to get order number")
+			w.WriteHeader(500)
+			w.Write([]byte("error"))
+		}
+
+		// send message
+		err = sendMessage(number)
 		if err != nil {
 			log.WithError(err).Warn("Unable to send message")
 			w.WriteHeader(500)
@@ -71,8 +82,37 @@ func CheckoutHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
 }
 
+// Get order number by calling the service
+func getOrderNumber() (number int, err error) {
+	client, err := dapr.NewClient()
+	if err != nil {
+		log.WithError(err).Error("Unable to create DAPR client")
+		return 0, err
+	}
+	defer client.Close()
+
+	ctx := context.Background()
+	defer ctx.Done()
+
+	response, err := client.InvokeMethod(ctx, "number", "number", "GET")
+	if err != nil {
+		log.WithError(err).Warn("Unable to get order number")
+		return 0, err
+	}
+
+	numberString := string(response)
+	number, err = strconv.Atoi(numberString)
+	if err != nil {
+		log.WithError(err).WithField("numberString", numberString).Warn("Unable to convert atoi")
+		return 0, err
+	}
+	log.WithField("number", number).Info("Received order number")
+
+	return number, nil
+}
+
 // Send a message to pubsub
-func sendMessage() error {
+func sendMessage(number int) error {
 	client, err := dapr.NewClient()
 	if err != nil {
 		log.WithError(err).Error("Unable to create DAPR client")
@@ -81,7 +121,10 @@ func sendMessage() error {
 	defer client.Close()
 
 	ctx := context.Background()
-	err = client.PublishEvent(ctx, pubsubName, topicName, []byte(data))
+	defer ctx.Done()
+	
+	data := []byte(strconv.Itoa(number))
+	err = client.PublishEvent(ctx, pubsubName, topicName, data)
 	if err != nil {
 		log.WithError(err).WithFields(
 			log.Fields{
